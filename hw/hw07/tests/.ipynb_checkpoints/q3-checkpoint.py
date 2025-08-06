a212e93c23a34f86d746e4287681c40b46100a95
wkKarry <1415546982@qq.com>
2025-08-06 22:28:02 +0800

    hw07完成! 重点是"标准化→SVD→PCA",可能需要看看 https://www.youtube.com/playlist?list=PLMrJAkhIeNNSVjnsviglFoY2nXildDCcv中的 https://www.youtube.com/watch?v=xy3QyyhiuY4&list=PLMrJAkhIeNNSVjnsviglFoY2nXildDCcv&index=3&t=2s和 https://youtu.be/fkf4IBRSeEc?si=S0Ug8BPVP9FRNotk 以辅助理解

---
+++ hw/hw07/tests/.ipynb_checkpoints/q3-checkpoint.py
@@ -0,0 +1,10 @@
+OK_FORMAT=True
+test = {   'name': 'q3',
+    'points': 3,
+    'suites': [   {   'cases': [   {'code': '>>> res_q3.shape == (2, 2)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': ">>> set(res_q3.columns) == set(['isBigHit', 'total'])\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> set(res_q3['isBigHit']) == set(['yes', 'no'])\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
